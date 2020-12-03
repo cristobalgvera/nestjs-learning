ac612a1442f4e088d772c8dee08d0672bf081312
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-12-02 21:52:18 -0300

    :art: General project structure refactor
    This way will be possible to easily manage API and frontend projects

--- task-management-api/src/main.ts
+++ task-management-api/src/main.ts
@@ -4,12 +4,15 @@ import { Logger } from '@nestjs/common';
 import { MAIN } from './config/context.names';
 import * as config from 'config';
 
+const { NODE_ENV } = process.env;
+
 async function bootstrap() {
   const { port } = config.get<any>('server');
   const listeningPort = process.env.PORT || port;
 
   const logger = new Logger(MAIN.BOOTSTRAP);
   const app = await NestFactory.create(AppModule);
+  if (NODE_ENV === 'development') app.enableCors();
   await app.listen(listeningPort);
 
   logger.log(`Application listening on port ${listeningPort}`);
